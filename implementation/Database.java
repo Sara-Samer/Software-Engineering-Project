683444975381c82fd12e68809373dd02b6912568
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2019-12-26 17:46:14 +0200

    Database singleton and login

--- implementation/Database.java
+++ implementation/Database.java
@@ -3,17 +3,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Database{
-	ArrayList<User> users = new ArrayList<User>();
-	Map<String , User> userToken = new HashMap<String , User>();
-	public void addUser(User user){
-		users.add(user);
-	}
+    ArrayList<User> users = new ArrayList<User>();
+    Map<String , User> userToken = new HashMap<String , User>();
+    private static Database instance = null;
 
-	public User getUserByEmail(String mail){
-		int index = users.indexOf(mail);
-		return index == -1? null : users.get(index);
-	}
-	public User getUserByToken(String token) {
-		return userToken.containsKey(token) ? userToken.get(token) : null;
-	}
+    private Database(){
+
+    }
+
+    public void addUser(User user){
+        users.add(user);
+    }
+
+    public User getUserByEmail(String mail){
+        int index = users.indexOf(mail);
+        return index == -1? null : users.get(index);
+    }
+
+    public User getUserByToken(String token) {
+        return userToken.containsKey(token) ? userToken.get(token) : null;
+    }
+
+    public static Database getInstance(){
+        if(instance == null)
+            instance = new Database();
+        return instance;
+    }
 }
--- implementation/UserAPI.java
+++ implementation/UserAPI.java
@@ -1,6 +1,6 @@
 class UserAPI{
-    static User signup(String firstname, String lastname, String email, String password, String birthdate, boolean is_male, Country country){
-        Database db = new Database();
+    static User signup(String firstname, String lastname, String email, String password, String birthdate, boolean is_male, Country country) throws Exception{
+        Database db = Database.getInstance();
         if(db.getUserByEmail(email) != null)
             throw new Exception("Email exists");
         if(firstname.trim().length() == 0)
@@ -18,11 +18,23 @@ class UserAPI{
         return user;
     }
 
-    static User login(String email, String password){return new User();}
-    static User getUserByToken(String token){return new User();}
+    static String login(String email, String password){
+        Database db = Database.getInstance();
+        User u = db.getUserByEmail(email);
+        if(u.password.equals(password)){
+            return db.getUserToken(u);
+        }
+        return null;
+    }
+
+    static User getUserByToken(String token){
+        Database db = Database.getInstance();
+        return db.getUserByToken(token);
+    }
+
     static User[] findUsersByName(String name){return new User[0];}
     static User findUserByEmail(String email){return new User();}
-    static PremiumUser upgradeUser(User user, PaymentAccount account){return new PremiumUser();}
+    static PremiumUser upgradeUser(User user, PaymentAccount account){}
     static boolean updateUserPassword(User user, String new_password){return false;}
     static boolean addFriend(User sender, User reciever){return false;}
 }
