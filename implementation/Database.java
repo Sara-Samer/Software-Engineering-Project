c9910c1bd86588baed80d36b4f1577444864f19c
salma27 <36279740+salma27@users.noreply.github.com>
2019-12-26 17:25:10 +0200

    Update Database.java

--- implementation/Database.java
+++ implementation/Database.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
 class Database{
 	ArrayList<User> users = new ArrayList<User>();
-	Map<String , User> UserToken = new HashMap<String , User>();
+	Map<String , User> userToken = new HashMap<String , User>();
 	public void addUser(User user){
 		users.add(user);
 	}
@@ -11,5 +12,8 @@ class Database{
 	public User getUserByEmail(String mail){
 		int index = users.indexOf(mail);
 		return index == -1? null : users.get(index);
+	}
+	public User getUserByToken(String token) {
+		return userToken.containsKey(token) ? userToken.get(token) : null;
 	}
 }
