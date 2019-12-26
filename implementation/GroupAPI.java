b608669b6568015853f3275db426a00a56f83063
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2019-12-26 11:36:00 +0200

    Create GroupAPI.java

---
+++ implementation/GroupAPI.java
@@ -0,0 +1,13 @@
+import java.awt.image.BufferedImage;
+
+public class  GroupAPI{
+	public static Group createGroup(String name, BufferedImage photo, String description, Privacy privacy, User admin){
+		
+	}
+	public static Group[] findGroupByName(String name) {
+		
+	}
+	public static Boolean joinGroup(User user, Group group) {
+		
+	}
+}
