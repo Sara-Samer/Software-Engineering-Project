6d5d6440ee39230d406457de25820ff53e0ffc0e
salma27 <36279740+salma27@users.noreply.github.com>
2019-12-26 17:00:41 +0200

    Update Database.java

--- implementation/Database.java
+++ implementation/Database.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 class Database{
 	ArrayList<User> users = new ArrayList<User>();
+	Map<String , User> UserToken = new HashMap<String , User>();
 	public void addUser(User user){
 		users.add(user);
 	}
+	
 }
