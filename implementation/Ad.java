865338e84e6e8a4540cb9739a25a52dae4dc50e5
salma27 <36279740+salma27@users.noreply.github.com>
2019-12-27 20:35:08 +0200

    Update Ad.java

--- implementation/Ad.java
+++ implementation/Ad.java
@@ -1,1 +1,24 @@
-class Ad {}
+class Ad {
+	private String header;
+	private String[] headLines;
+	private String[] textDescription;
+	public Ad() {
+		header = "";
+		headLines = new String[2];
+		textDescription = new String[4];
+	}
+	public void setHeader(String header) {
+		this.header = header;
+	}
+	public void setHeadLines(String[] headLines) {
+		for(int i = 0 ; i < 2 ; i++)
+			this.headLines[i] = headLines[i];
+	}
+	public void setTextDescription(String[] textDescription) {
+		for(int i = 0 ; i < 4 ; i++)
+			this.textDescription[i] = textDescription[i];
+	}
+	public String getHeader() {return this.header;}
+	public String[] getHeadLines() {return this.headLines;}
+	public String[] getTextDescription() {return this.textDescription;}
+}
