41c675e45bbb70ae6d5ca52f97ecf1798ab68770
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2019-12-27 19:23:27 +0200

    create payment account using a constructor

--- implementation/PaymentAccount.java
+++ implementation/PaymentAccount.java
@@ -1,4 +1,8 @@
 class PaymentAccount {
     String creditCardNumber;
     String holderName;
+    public PaymentAccount(String creditCardNumber, String holderName) {
+    	this.creditCardNumber = creditCardNumber;
+    	this.holderName = holderName;
+    }
 }
